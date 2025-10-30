f1e7e76c5286a80655d7d75b2ec9fc11b4545f19
julicarolinav <julicarolinav@gmail.com>
2025-10-30 11:18:48 -0500

    websocket

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1643,7 +1643,7 @@ const handleLoadGeofence = (geofenceData) => {
                     
                     <VideoStream
                     deviceId={selectedDeviceForVideo}
-                    serverUrl={`${window.location.protocol === 'https:' ? 'wss' : 'ws'}://${config.API_BASE_URL.replace('http://', '').replace('https://', '').split(':')[0]}:8443`}
+                    serverUrl={`${window.location.protocol === 'https:' ? 'wss' : 'ws'}://${config.API_BASE_URL.replace('http://', '').replace('https://', '').split(':')[0]}:8080`}
                     />
                     
                     <div className="mt-4 flex gap-3">
